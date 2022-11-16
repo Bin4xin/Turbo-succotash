0e28cddf787527ceec97987276d2af48028d24f1
bin4xin <57884471+Bin4xin@users.noreply.github.com>
2022-11-16 21:46:22 +0800

    [2022年11月16日 21:45:37] < os_detected by platform to run different command > by bin4xin

--- map_game/lib/clear.py
+++ map_game/lib/clear.py
@@ -1,9 +1,19 @@
 import os
+import platform
 
 
 def clear():
-    # os.system('clear')
-    os.system('cls')
+    if os_detected() != "Windows":
+        os.system('clear')
+    else:
+        os.system('cls')
+        """
+        flush function in terminal.  @link{ https://www.zhihu.com/question/293719659 }
+        """
+
+
+def os_detected():
     """
-    flush function in terminal.  @link{ https://www.zhihu.com/question/293719659 }
+    platform detected by python libs <platform> @link{ https://blog.csdn.net/gatieme/article/details/45674367 }
     """
+    return platform.system()
