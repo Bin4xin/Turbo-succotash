48d8d6d3cb526229e87a986769b9c933fc2fd79a
bin4xin <57884471+Bin4xin@users.noreply.github.com>
2022-11-16 21:47:11 +0800

    [2022年11月16日 21:45:37] < replace true or false from str judgement > by bin4xin

--- map_game/lib/judge.py
+++ map_game/lib/judge.py
@@ -1,8 +1,8 @@
-def judge(map, x, y):
-    # print(x, y)
-    if map[x][y] == " ":
-        map[x][y] = "O"
-    else:
-        print("你撞墙啦")
-        # return
-        exit()  # TODO: return 撞墙之前的x y值，游戏继续。
+def allow_Pass_orNot(map, x, y):
+    flag = "true"
+    for index in map:
+        if map[x][y] == "#":
+            return flag
+        elif map[x][y] != "#":
+            flag = "false"
+            return flag
